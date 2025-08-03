e18adfb1fa6e27723024b936d717c449771fe6aa
Kseniia Riabova <kseniaistratova@gmail.com>
2025-08-03 14:02:19 +0200

    Add Redis-backed rate limiting to Express app
    Integrated express-rate-limit with RedisStore to limit each IP to 5 requests per minute.
    
    This helps prevent abuse and improves API reliability.

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
-// const rateLimit = res;
+const rateLimit = require('express-rate-limit');
+const { RedisStore } = require('rate-limit-redis');
+const { createClient } = require('redis');
+
+const redisClient = createClient();
+redisClient.connect().catch(console.error);
+
+const limiter = rateLimit({
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.sendCommand(args),
+  }),
+  windowMs: 60 * 1000, // 1 minute
+  max: 5, // liming each IP to 5 requests per windowMs
+  message: 'Too many requests, please try again later.',
+});
 
 const fakeDB = () =>
   new Promise((res) => {
@@ -15,6 +29,6 @@ app.get('/user', async (req, res) => {
   res.json(data);
 });
 
-app.listen(3001, () =>
-  console.log('Server is running on http://localhost:3001')
-);
+app.listen(3001, () => {
+  console.log('Server is running on http://localhost:3001');
+});
